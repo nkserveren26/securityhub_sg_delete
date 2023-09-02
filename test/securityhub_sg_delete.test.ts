9d6473df24a13e957078791d563846e79ee03795
nkserveren26 <norinoritoshi@outlook.jp>
2023-09-02 14:24:41 +0900

    commit

---
+++ test/securityhub_sg_delete.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SecurityhubSgDelete from '../lib/securityhub_sg_delete-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/securityhub_sg_delete-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SecurityhubSgDelete.SecurityhubSgDeleteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
