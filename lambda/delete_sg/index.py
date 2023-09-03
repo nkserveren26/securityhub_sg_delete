f1ff09ca4b8c794c8be94da130af6218b36cf609
nkserveren26 <norinoritoshi@outlook.jp>
2023-09-04 00:16:29 +0900

    commit

--- lambda/delete_sg/index.py
+++ lambda/delete_sg/index.py
@@ -1,6 +1,21 @@
 import boto3
+from botocore.exceptions import ClientError
 
 ec2 = boto3.client('ec2')
 
 def handler(event, context):
     print(event)
+    #セキュリティグループIDを取得
+    sgID = ''
+
+    #セキュリティグループの削除を実行
+    try:
+        response = ec2.revoke_security_group_ingress(
+            CidrIp = '0.0.0.0/0',
+            GroupId = sgId,
+            FromPort = 3389,
+            ToPort = 3389,
+            IpProtocol = 'tcp'
+        )
+    except ClientError as e:
+        print(e)
