8e0453770d52d57e775e493e6da37ae784daab11
nkserveren26 <norinoritoshi@outlook.jp>
2023-09-10 16:54:51 +0900

    commit

--- lambda/delete_sg/index.py
+++ lambda/delete_sg/index.py
@@ -6,8 +6,9 @@ ec2 = boto3.client('ec2')
 
 def handler(event, context):
     print(event)
-    resources = event["detail"]["findings"]["Resources"]["Details"]
-    print(resources)
+    resources = event["detail"]["findings"][0]["Resources"]
+    sgId = resources[0]["Details"]["AwsEc2SecurityGroup"]["GroupId"]
+    print(sg)
     #セキュリティグループIDを取得
     #message = json.loads(event['Records'][0])
 
