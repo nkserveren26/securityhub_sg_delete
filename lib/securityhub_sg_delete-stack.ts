227a8075c4aee6e27e726ecce1ccdf61e9db9278
nkserveren26 <norinoritoshi@outlook.jp>
2023-09-11 22:45:27 +0900

    commit

--- lib/securityhub_sg_delete-stack.ts
+++ lib/securityhub_sg_delete-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import { Function } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import { LambdaCreator } from './services/lambda/creator';
+import { LambdaFunctionParams } from './services/lambda/interfaces';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SecurityhubSgDeleteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const delete_sg_func_params: LambdaFunctionParams = {
+      functionName: "",
+      description: "This lambda deletes inbound rules which allow full open.",
+      codePath: "lambda/delete_sg",
+      handler: "index.handler"
+    };
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SecurityhubSgDeleteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const delete_sg_func: Function = LambdaCreator.createLambdaFunction(
+      scope, 
+      delete_sg_func_params
+    );
   }
 }
