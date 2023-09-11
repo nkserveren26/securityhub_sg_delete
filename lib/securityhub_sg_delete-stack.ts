09c2ec1be0838d340d0f0a7e88d1ee36f252c2b8
nkserveren26 <norinoritoshi@outlook.jp>
2023-09-11 23:03:49 +0900

    commit

--- lib/securityhub_sg_delete-stack.ts
+++ lib/securityhub_sg_delete-stack.ts
@@ -3,6 +3,7 @@ import { Function } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { LambdaCreator } from './services/lambda/creator';
 import { LambdaFunctionParams } from './services/lambda/interfaces';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SecurityhubSgDeleteStack extends cdk.Stack {
@@ -15,6 +16,12 @@ export class SecurityhubSgDeleteStack extends cdk.Stack {
       codePath: "lambda/delete_sg",
       handler: "index.handler"
     };
+
+    const policy_for_lambdaRole: PolicyStatement = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["ec2:RevokeSecurityGroupIngress"],
+      resources: ["*"]
+    });
 
     const delete_sg_func: Function = LambdaCreator.createLambdaFunction(
       this, 
