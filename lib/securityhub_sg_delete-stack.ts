aea7006c03ceb4044ecbed6398a4ba6dcc0ff8ab
nkserveren26 <norinoritoshi@outlook.jp>
2023-09-14 21:40:42 +0900

    commit

--- lib/securityhub_sg_delete-stack.ts
+++ lib/securityhub_sg_delete-stack.ts
@@ -4,7 +4,7 @@ import { Construct } from 'constructs';
 import { LambdaCreator } from './services/lambda/creator';
 import { LambdaFunctionParams } from './services/lambda/interfaces';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { CfnEventBus } from 'aws-cdk-lib/aws-events';
+import { CfnEventBus, CfnRule } from 'aws-cdk-lib/aws-events';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SecurityhubSgDeleteStack extends cdk.Stack {
@@ -20,6 +20,8 @@ export class SecurityhubSgDeleteStack extends cdk.Stack {
 
     const eventBridgeParams = {
       eventBusName: "eventBus",
+      ruleName: "securityHubAllPortRule",
+      ruleDescription: "A rule for security hub policy stricting restricting the opening of all ports"
     };
 
     const policy_for_lambdaRole: PolicyStatement = new PolicyStatement({
@@ -36,8 +38,18 @@ export class SecurityhubSgDeleteStack extends cdk.Stack {
     //LambdaのロールにSGのインバウンドルール削除権限を付与するポリシーを追加
     delete_sg_func.addToRolePolicy(policy_for_lambdaRole);
 
+    //EventBridgeの作成
     const eventBus = new CfnEventBus(this, eventBridgeParams.eventBusName,{
       name: eventBridgeParams.eventBusName
     });
+
+    const rule = new CfnRule(this, eventBridgeParams.ruleName, {
+      name: eventBridgeParams.ruleName,
+      description: eventBridgeParams.ruleDescription,
+      eventBusName: eventBus.attrName,
+    
+    });
+
+
   }
 }
