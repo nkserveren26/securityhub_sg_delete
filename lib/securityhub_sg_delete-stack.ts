c61624703ff0caad7c7612fa86ff449a8eee3b47
nkserveren26 <norinoritoshi@outlook.jp>
2023-09-14 21:47:59 +0900

    commit

--- lib/securityhub_sg_delete-stack.ts
+++ lib/securityhub_sg_delete-stack.ts
@@ -54,6 +54,24 @@ export class SecurityhubSgDeleteStack extends cdk.Stack {
         "source": [
           "aws.securityhub"
         ],
+        "detail": {
+          "findings": {
+            "ProductArn": ["arn:aws:securityhub:ap-northeast-1::product/aws/securityhub"],
+            "GeneratorId": ["security-control/EC2.19"],
+            "Severity": {
+              "Label": ["CRITICAL"]
+            },
+            "Compliance": {
+              "Status": [{
+                "anything-but": "PASSED"
+              }]
+            },
+            "Workflow": {
+              "Status": ["NEW"]
+            },
+            "RecordState": ["ACTIVE"]
+          }
+        },
       }
     
     });
