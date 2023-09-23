a44513d32e9a134bda48535f4f88bd077c1bcdb0
nkserveren26 <norinoritoshi@outlook.jp>
2023-09-23 16:19:51 +0900

    commit

--- lib/services/eventbridge/creator.ts
+++ lib/services/eventbridge/creator.ts
@@ -1,3 +1,8 @@
+import { CfnRule } from "aws-cdk-lib/aws-events";
+
 export class EventBridgeCreator {
-    
+    public static createEventRule(): CfnRule {
+        const eventRule = new CfnRule();
+        return eventRule;
+    }
 }
